997ace5c90894e6f6b02687774abc0088813b9e4
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-18 13:01:51 +0000

    Config: install and configure DaisyUI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-600 underline">TribeHub</h1>
+      <h1 className="text-red-600 underline">TribeHub</h1>
+      <div className="flex justify-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <h2>A DaisyUI card</h2>
+          <div className="card-body">
+            <div className="card-title">
+              <h3>A DaisyUI card title</h3>
+            </div>
+            <p>Some text</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
