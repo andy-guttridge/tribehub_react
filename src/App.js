d5e0dfffc4700ee71b37f4b4533f16bd3f3aefa8
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-18 16:46:36 +0000

    Fix: correct filename for navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
-import NavBar from './components/BottomNavBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
