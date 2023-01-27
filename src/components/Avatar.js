e015b4fdf37be1a5def988210a43a8e318d5dc27
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-27 19:39:41 +0000

    Feature: add option to display a larger avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Avatar({ imageUrl }) {
+function Avatar({ imageUrl, large }) {
   return (
     <div className="avatar m-4 inline">
-      <div className="w-20 rounded-full">
+      <div className={(large ? "w-40" : "w-20") + " rounded-full"}>
         <img src={imageUrl}></img>
       </div>
     </div>
