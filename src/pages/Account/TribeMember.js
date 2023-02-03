0cdedaeaa54731f0c60229fca7cf1efad4ec3524
Andy Guttridge <andyguttridge123@gmail.com>
2023-02-03 09:32:43 +0000

    Refactor: minor change to border style

--- src/pages/Account/TribeMember.js
+++ src/pages/Account/TribeMember.js
@@ -5,7 +5,7 @@ import Avatar from '../../components/Avatar'
 function TribeMember({ tribeMember, handleDeleteButton }) {
 
   return (
-    <div className="border-2 rounded-sm grid grid-cols-2 w-4/5 md:w-2/3 lg:1/2 mx-auto my-4">
+    <div className="border-b-2 rounded-sm grid grid-cols-2 w-4/5 md:w-2/3 lg:1/2 mx-auto my-4">
       <div className="flex col-start-1 justify-content-between">
         <Avatar imageUrl={tribeMember.profile_image} />
         <span className="basis-1 m-2" aria-label="Name of tribe member">{tribeMember.display_name}</span>
