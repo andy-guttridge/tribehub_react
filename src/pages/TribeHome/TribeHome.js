c80629a20bf3d4338c2d33dd1b2d75ecabdaa1e1
Andy Guttridge <andyguttridge123@gmail.com>
2023-02-04 17:11:47 +0000

    Refactor: merge TribeCal and TribeHome components

--- src/pages/TribeHome/TribeHome.js
+++ src/pages/TribeHome/TribeHome.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import { Calendar } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 import { useNavigate } from 'react-router-dom'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
 import { useSinglePage } from '../../contexts/SinglePageContext';
-
-import TribeCal from './TribeCal';
+import styles from '../../styles/Calendar.module.css';
+import Spinner from '../../components/Spinner';
+import { axiosReq } from '../../api/axiosDefaults';
+import { checkEventsForDate, getEventsForDay } from '../../utils/utils';
 import CalEvent from './CalEvent';
 
 function TribeHome() {
+
+  // State variables for loading status
+  const [hasLoaded, setHasLoaded] = useState(false)
+
   // Get ref to current user
   const currentUser = useCurrentUser();
 
@@ -21,20 +28,69 @@ function TribeHome() {
   // Styles to apply if app is in single page mode
   const singlePageStyles = "basis-4/5 border-solid border-2 m-2";
 
+  // State variables for user's events
+  const [events, setEvents] = useState({ results: [] });
+
   // State variables for specific day's events
   const [dayEvents, setDayEvents] = useState([]);
 
-  const handleCalDaySelect = (events) => {
-    setDayEvents(events);
+  // Fetch user's events
+  const fetchEvents = async (fromDate, toDate) => {
+
+    // Convert fromDate and toDate to ISO strings for the API, and get rid of last 5 chars to eliminate timezone data
+    const toDateStr = toDate.toISOString().slice(0, -5);
+    const fromDateStr = fromDate.toISOString().slice(0, -5);
+    try {
+      const { data } = await axiosReq.get(`/events/?from_date=${fromDateStr}&to_date=${toDateStr}`)
+      setEvents(data);
+      setHasLoaded(true);
+    }
+    catch (errors) {
+      return errors
+    }
   }
+
+  useEffect(() => {
+    // Set dates for fetching calendar data. Load data for 3 months before and after today.
+    const fromDate = new Date();
+    fromDate.setMonth(fromDate.getMonth() - 12);
+    const toDate = new Date();
+    toDate.setMonth(toDate.getMonth() + 12);
+
+    // Fetch the data
+    try {
+      fetchEvents(fromDate, toDate)
+    }
+    catch (errors) {
+      console.log(errors);
+    }
+  }, [])
 
   useEffect(() => {
     // Check if user logged in on mount, if not redirect to landing page
     !currentUser && navigate("/");
-  }, [currentUser, dayEvents])
+  }, [currentUser])
+
+  // Handle the user changing the calendar month by reloading events data with correct date range
+  const handleCalMonthChange = (calData) => {
+
+    // Set dates for fetching data based on the activeStartDate supplied by the calendar component
+    const { activeStartDate } = calData;
+    const fromDate = new Date(activeStartDate.getTime());
+    fromDate.setMonth(activeStartDate.getMonth() - 12);
+    const toDate = new Date(activeStartDate.getTime());
+    toDate.setMonth(toDate.getMonth() + 12);
+
+    // Fetch the data
+    try {
+      fetchEvents(fromDate, toDate);
+    }
+    catch (errors) {
+      console.log(errors);
+    }
+  }
 
   return (
-
     // Apply some styling if displaying in single page mode
     <div
       className={
@@ -42,15 +98,34 @@ function TribeHome() {
       }
     >
       <h2>Home</h2>
-      <TribeCal handleCalDaySelect={handleCalDaySelect} />
-      <div className="max-h-96 overflow-scroll">
-        {
-          dayEvents?.map((dayEvent) => {
-            return <CalEvent event={dayEvent} key={`event-${dayEvent.id}`} />
-          })
-        }
-      </div>
-    </div>
+      {hasLoaded ? (
+
+        // Calendar
+        <>
+          <div className="inline-block m-4">
+            <Calendar
+              className={`${styles.TribehubCalendar}`}
+              calendarType="ISO 8601"
+              minDetail="month"
+              tileContent={(calData) => checkEventsForDate(calData, events)}
+              onActiveStartDateChange={handleCalMonthChange}
+              onClickDay={(calDate, event) => setDayEvents(getEventsForDay(calDate, events, event))}
+            />
+
+            {/* Event details for selected day */}
+            <div className="max-h-96 overflow-scroll">
+              {
+                dayEvents?.map((dayEvent, i) => {
+                  return <CalEvent event={dayEvent} key={`event-${dayEvent.id}`} />
+                })
+              }
+            </div>
+          </div>
+        </>
+      ) : (
+        <Spinner />
+      )}
+    </ div>
   )
 }
 
