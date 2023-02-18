e2711c8fb97032b393dfd1cb0e8baffd35e058df
Andy Guttridge <andyguttridge123@gmail.com>
2023-02-18 18:37:18 +0000

    Feature: add form to add new contacts

---
+++ src/pages/Contacts/ContactDetailsForm.js
@@ -0,0 +1,238 @@
+import React, { useState } from 'react'
+import { InfoCircle } from 'react-bootstrap-icons';
+import { axiosReq } from '../../api/axiosDefaults';
+
+function ContactDetailsForm({ handleCancelButton, didSaveContact, setDidSaveContact }) {
+
+  // State variables for form values
+  const [contactForm, setContactForm] = useState({
+    category: '',
+    company: '',
+    title: '',
+    first_name: '',
+    last_name: '',
+    phone: '',
+    email: '',
+  })
+
+  // Retrieve form data from state variables
+  const { category, company, title, first_name, last_name, phone, email } = contactForm;
+
+  // State variables for errors
+  const [errors, setErrors] = useState({});
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axiosReq.post('contacts/', contactForm);
+
+      // Hide form and tell parent component the contact was saved
+      handleCancelButton();
+      setDidSaveContact(!didSaveContact);
+      setErrors({});
+    }
+    catch (error) {
+      setErrors(error.response?.data);
+    }
+  }
+
+  const handleChange = (e) => {
+    setContactForm({
+      ...contactForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return (
+    <div className="w-4/5 m-auto">
+      <h3>Add contact</h3>
+      <form onSubmit={handleSubmit}>
+
+        {/* Category field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="category">
+          <span>Category:</span>
+          <input
+            type="text"
+            className="input input-bordered w-full"
+            id="category"
+            name="category"
+            value={category}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any category field errors */}
+        {
+          errors?.category?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`category-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Company field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="company">
+          <span>Company:</span>
+          <input
+            type="text"
+            className="input input-bordered w-full"
+            id="company"
+            name="company"
+            value={company}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any company field errors */}
+        {
+          errors?.company?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`company-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Title field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="title">
+          <span>Title:</span>
+          <input
+            type="title"
+            className="input input-bordered w-full"
+            id="title"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any title field errors */}
+        {
+          errors?.title?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`title-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* First name field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="first_name">
+          <span>First name:</span>
+          <input
+            type="text"
+            className="input input-bordered w-full"
+            id="first_name"
+            name="first_name"
+            value={first_name}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any first name field errors */}
+        {
+          errors?.first_name?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`first_name-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Last name field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="last_name">
+          <span>Last name:</span>
+          <input
+            type="text"
+            className="input input-bordered w-full"
+            id="last_name"
+            name="last_name"
+            value={last_name}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any last name field errors */}
+        {
+          errors?.last_name?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`last_name-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Phone number field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="phone">
+          <span>Telephone number:</span>
+          <input
+            type="tel"
+            className="input input-bordered w-full"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* Display alert with any phone number field errors */}
+        {
+          errors?.phone?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`phone-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Email field */}
+        <label className="input-group max-lg:input-group-vertical mb-4" htmlFor="email">
+          <span>Email:</span>
+          <input
+            type="email"
+            className="input input-bordered w-full"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+        
+        {/* Display alert with any email field errors */}
+        {
+          errors?.email?.map((error, i) => {
+            return (
+              <div className="alert alert-warning justify-start mt-4 mb-4" key={`email-err${i}`}>
+                <InfoCircle size="32" /><span>{error}</span>
+              </div>
+            )
+          })
+        }
+
+        {/* Cancel and submit buttons */}
+        <button className="btn btn-outline" onClick={handleCancelButton}>Cancel</button>
+        <button className="btn btn-outline w-1/3 m-2" type="submit">Submit</button>
+
+        {/* Display alert with any non-field errors */}
+        {
+          errors?.non_field_errors?.map((error, i) => (
+            <div className="alert alert-warning justify-start mt-4" key={`contact-nonfield-err${i}`}>
+              <InfoCircle size="32" /><span>{error}</span>
+            </div>
+          ))
+        }
+
+      </form>
+    </div>
+  )
+}
+
+export default ContactDetailsForm
--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { PlusCircle } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom'
 import { axiosReq } from '../../api/axiosDefaults';
 import Spinner from '../../components/Spinner';
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
 import { useSinglePage } from '../../contexts/SinglePageContext';
 import Contact from './Contact';
+import ContactDetailsForm from './ContactDetailsForm';
 
 function Contacts() {
 
@@ -21,13 +23,20 @@ function Contacts() {
   const singlePageStyles = "basis-4/5 border-solid border-2 flex-none m-2"
 
   // State variables for tribe contacts
-  const [contacts, setContacts] = useState({});
+  const [contacts, setContacts] = useState([]);
 
   // State variables to record if data has loaded
   const [hasLoaded, setHasLoaded] = useState(false);
 
   // State variables for errors
   const [errors, setErrors] = useState({});
+
+  // State variables for whether user is adding a contact
+  const [isAddingContact, setIsAddingContact] = useState(false);
+
+  // State variable used as a flag when contact details are saved.
+  // This is simply toggles to trigger events to reload when there has been a change.
+  const [didSaveContact, setDidSaveContact] = useState(false);
 
   // Check if user logged in on mount, if not redirect to landing page
   useEffect(() => {
@@ -37,19 +46,19 @@ function Contacts() {
   // Fetch users contacts from API
   useEffect(() => {
     const fetchContacts = async () => {
+      console.log(currentUser.username)
       try {
         setHasLoaded(false);
         const { data } = await axiosReq.get('contacts/');
         setContacts(data);
         setHasLoaded(true);
-        console.log(data.results);
       }
       catch (error) {
         setErrors(errors);
       }
     }
     fetchContacts();
-  }, [])
+  }, [didSaveContact])
 
   return (
     // Apply some styling if displaying in single page mode
@@ -59,20 +68,49 @@ function Contacts() {
       }
     >
       <h2>Contacts</h2>
+
+      {/* Display contacts */}
       {
         hasLoaded ? (
           <div className="inline-block w-4/5">
-        {
-          contacts?.results?.map((contact) => {
-            return <Contact contact={contact} />
-          })
-        }
-      </div>
+            {/* Display message if there are no tribe contacts yet */}
+
+            {
+              !contacts?.results?.length && <p className="text-left md:text-center">Press the + button to start adding your tribe's contacts</p>
+            }
+
+            {
+              contacts?.results?.map((contact) => {
+                return <Contact contact={contact} key={contact.id} />
+              })
+            }
+          </div>
         ) : (
           <Spinner />
         )
       }
-      
+
+      {/* Show add contact button if user is tribe admin and they are not currently adding a contact */}
+      <div className="justify-end flex w-4/5 md:w-2/3 lg:1/2 mx-auto my-4">
+        {!isAddingContact && currentUser.is_admin &&
+          <button className='btn btn-ghost'
+            onClick={() => setIsAddingContact(!isAddingContact)}>
+            <PlusCircle size="32" />
+            <span className="sr-only">Add new contact</span>
+          </button>
+        }
+      </div>
+
+      {/* Show contact details form if user is currently adding a contact */}
+      {
+        isAddingContact && currentUser.is_admin &&
+        <ContactDetailsForm
+          handleCancelButton={() => setIsAddingContact(!isAddingContact)}
+          didSaveContact={didSaveContact}
+          setDidSaveContact={setDidSaveContact}
+        />
+      }
+
     </div>
   )
 }
