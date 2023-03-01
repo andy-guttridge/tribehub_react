3b3715a6ad6a6c355338f9cb49b9e3b8029f4b24
Andy Guttridge <andyguttridge123@gmail.com>
2023-03-01 16:00:10 +0000

    Fix: style submit buttons on register and login pages to match other submit buttons

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -90,7 +90,7 @@ function Signin() {
                 onChange={handleChange}
               />
             </label>
-            <button className="btn btn-outline w-full" type="submit">Submit</button>
+            <button className="btn btn-primary w-full" type="submit">Submit</button>
           </form>
 
           {/* Display alert with any sign-in errors */}
