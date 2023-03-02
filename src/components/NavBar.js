25b586d458ea636637247f8e446801443066336b
Andy Guttridge <andyguttridge123@gmail.com>
2023-03-02 12:22:35 +0000

    Fix: correctly highlight currently selected nav item

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,21 +8,24 @@ function NavBar() {
 
   // Reference to current user
   const currentUser = useCurrentUser();
+
+  // Reference to current URL
+  const location = useLocation();
   
   // Icons for bottom navbar (only used for breakpoints below large). Use current url to apply the active class to the correct Navlink component
   const navBarIcons = (
     <>
-      <NavLink  to="/tribe-home" className={`bg-base-200 {location.pathname !== '/account' && location.pathname !=='/contacts' && 'active'}`}>
+      <NavLink  to="/tribe-home" className={`bg-base-200 ${location.pathname !== '/account' && location.pathname !=='/contacts' && 'active'}`}>
         <House size="32" aria-label="Home" />
         <span className="sr-only">Home</span>
       </NavLink>
 
-      <NavLink to="/contacts" className={`bg-base-200 {location.pathname === '/contacts' && 'active'}`}>
+      <NavLink to="/contacts" className={`bg-base-200 ${location.pathname === '/contacts' && 'active'}`}>
         <PersonVcard size="32" aria-label="Contacts" />
         <span className="sr-only">Contacts</span>
       </NavLink>
 
-      <NavLink to="/account" className={`bg-base-200 {location.pathname === '/account' && 'active'}`}>
+      <NavLink to="/account" className={`bg-base-200 ${location.pathname === '/account' && 'active'}`}>
         <PersonGear size="32" aria-label="Account" />
         <span className="sr-only">Account</span>
       </NavLink>
