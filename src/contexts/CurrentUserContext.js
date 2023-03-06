f495edd5a6fde5a262df8d9a3046846c367c40ce
Andy Guttridge <andyguttridge123@gmail.com>
2023-03-06 11:53:35 +0000

    Feature: add tribename to TribeHome

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -27,7 +27,7 @@ export const CurrentUserProvider = ({ children }) => {
       const {data} = await axiosRes.get('dj-rest-auth/user/');
       setCurrentUser(data);
     } catch(error) {
-      console.log(error);
+    console.log(error);
     }
   }
 
