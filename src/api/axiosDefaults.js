b41a3bfd94012db896a2062a65ddcfc174a533c6
Andy Guttridge <andyguttridge123@gmail.com>
2023-03-09 14:23:43 +0000

    Config: remove axios URL for locally hosted API server

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 // Configuration for axios
 axios.defaults.baseURL = 'https://tribehub-drf.herokuapp.com/';
-// axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
