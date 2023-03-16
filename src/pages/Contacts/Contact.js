9ac6d077150fddbb3ef0469459fb2832fe6b20ee
Andy Guttridge <andyguttridge123@gmail.com>
2023-03-16 12:15:33 +0000

    Fix: use two columns in Contact.js for all breakpoints to eliminate bad text overflow behaviour

--- src/pages/Contacts/Contact.js
+++ src/pages/Contacts/Contact.js
@@ -28,7 +28,7 @@ function Contact({ contact, didSaveContact, setDidSaveContact, handleDeleteButto
       }
 
       {/* Display contact details */}
-      <div className="card-body grid grid-cols-1 md:grid-cols-2 text-left break-words">
+      <div className="card-body grid grid-cols-2 text-left break-all">
         <div>
           <h3 className="text-xl">{contact.category}</h3>
           <p>{contact.title} {contact.first_name} {contact.last_name}</p>
